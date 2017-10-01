00bf4d22aa3d7001b3db88b37c1657c55cbfdf47
Matthew James <matthewjames@Matthews-MacBook-Pro.local>
2017-10-01 23:55:40 +1100

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## serverless-offline-sns-example
+
+Simply run
+```bash
+npm install
+npm start
+```
+
+And then go to http://localhost:3000/ping to trigger the event, you should see "pong" appear in the console.
---
+++ handler.js
@@ -0,0 +1,30 @@
+'use strict';
+var AWS = require("aws-sdk");
+
+module.exports.ping = (event, context, callback) => {
+  var sns = new AWS.SNS({
+    endpoint: "http://127.0.0.1:4002",
+    region: "us-east-1",
+  });
+  sns.publish({
+    Message: "hello!",
+    MessageStructure: "json",
+    TopicArn: "arn:aws:sns:us-east-1:123456789012:test-topic",
+  }, () => {
+    console.log("ping");
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'Ping sent!',
+        input: event,
+      }),
+    };
+
+    callback(null, response);
+  });
+};
+
+module.exports.pong = (event, context, callback) => {
+  console.log("pong");
+  callback(null, {});
+};
