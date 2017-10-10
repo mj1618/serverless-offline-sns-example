ad3b23058e54ad12ad7081b8eb2ae74f8bef6722
Matthew James <matthew.stephen.james@gmail.com>
2017-10-10 23:08:21 +0800

    Dynamo local integration example

--- handler.js
+++ handler.js
@@ -1,13 +1,23 @@
 'use strict';
 var AWS = require("aws-sdk");
+const dd = new AWS.DynamoDB.DocumentClient({
+  region: 'localhost',
+  endpoint: 'http://localhost:8000'
+})
 
 module.exports.ping = (event, context, callback) => {
+  console.log("context");
+  console.log(context);
   var sns = new AWS.SNS({
     endpoint: "http://127.0.0.1:4002",
     region: "us-east-1",
   });
+  dd.put({
+    'TableName': 'usersTable',
+    'Item': {email: 'adam@west.com'}
+ }, function(err, data) {
   sns.publish({
-    Message: "hello!",
+    Message: JSON.stringify({foo: "bar"}),
     MessageStructure: "json",
     TopicArn: "arn:aws:sns:us-east-1:123456789012:test-topic",
   }, () => {
@@ -22,9 +32,26 @@ module.exports.ping = (event, context, callback) => {
 
     callback(null, response);
   });
+ });
 };
 
 module.exports.pong = (event, context, callback) => {
+  console.log(context);
   console.log("pong");
-  callback(null, {});
+  console.log(event.Records[0].Sns.Message);
+  var params = {
+    TableName: "usersTable",
+    KeyConditionExpression: "#email = :email",
+    ExpressionAttributeNames:{
+        "#email": "email"
+        },
+    ExpressionAttributeValues: {
+        ":email":"adam@west.com"
+        }
+    };
+    dd.query(params,function(err, data){
+      console.log('dynamo result: ');
+      console.log(data);
+      callback(null, {});
+    });
 };
