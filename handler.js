27b9321838f6ffa3350a103290afb502e1a1dbd8
Matthew James <matthew.stephen.james@gmail.com>
2017-12-02 11:12:22 +0800

    Arbitrary regions example

--- handler.js
+++ handler.js
@@ -4,12 +4,12 @@ var AWS = require("aws-sdk");
 module.exports.ping = (event, context, callback) => {
   var sns = new AWS.SNS({
     endpoint: "http://127.0.0.1:4002",
-    region: "us-east-1",
+    region: "localhost",
   });
   sns.publish({
     Message: "hello!",
     MessageStructure: "json",
-    TopicArn: "arn:aws:sns:us-east-1:123456789012:test-topic",
+    TopicArn: "arn:aws:sns:localhost:123456789012:test-topic",
   }, () => {
     console.log("ping");
     const response = {
